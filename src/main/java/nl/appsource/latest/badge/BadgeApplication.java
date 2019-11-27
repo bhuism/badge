b9a310ccf00bc8654038506088bcd613a7714884
Bas Huisman <bhuism@odee.net>
2019-11-27 13:50:04 +0100

    add security

--- src/main/java/nl/appsource/latest/badge/BadgeApplication.java
+++ src/main/java/nl/appsource/latest/badge/BadgeApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,6 +17,19 @@ public class BadgeApplication {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf(csrf -> csrf.disable());
+            http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+            http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        }
+
     }
 
     public static void main(String[] args) {
