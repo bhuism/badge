6dd45c887b4383250792c68d71cc9dad583ae804
Bas Huisman <bhuism@odee.net>
2019-12-03 21:53:44 +0100

    remove autoconf

--- src/main/java/nl/appsource/latest/badge/BadgeApplication.java
+++ src/main/java/nl/appsource/latest/badge/BadgeApplication.java
@@ -1,17 +1,30 @@
 package nl.appsource.latest.badge;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+@Configuration
+@Import({
+        DispatcherServletAutoConfiguration.class,
+        RestTemplateAutoConfiguration.class,
+        ServletWebServerFactoryAutoConfiguration.class,
+        WebMvcAutoConfiguration.class,
+})
+@ComponentScan(basePackageClasses = BadgeApplication.class)
 public class BadgeApplication {
 
     @Bean
@@ -21,11 +34,11 @@ public class BadgeApplication {
 
     @Configuration
     @EnableWebSecurity
-    public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+    public static class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
-            http.csrf(csrf -> csrf.disable());
+            http.csrf(AbstractHttpConfigurer::disable);
             http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
             http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         }
