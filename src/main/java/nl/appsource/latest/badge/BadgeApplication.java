bb8406f371b561fcc8e6b7a66c68066338bc847b
Bas Huisman <bhuism@odee.net>
2019-12-04 14:28:12 +0100

    cleanups

--- src/main/java/nl/appsource/latest/badge/BadgeApplication.java
+++ src/main/java/nl/appsource/latest/badge/BadgeApplication.java
@@ -53,16 +53,14 @@ public class BadgeApplication {
     public static class Slf4jMDCFilterConfiguration {
 
         public static final String DEFAULT_RESPONSE_TOKEN_HEADER = "X-Badge-Request-Token";
-        public static final String DEFAULT_MDC_UUID_TOKEN_KEY = "Slf4jMDCFilter.UUID";
 
         private String responseHeader = DEFAULT_RESPONSE_TOKEN_HEADER;
-        private String mdcTokenKey = DEFAULT_MDC_UUID_TOKEN_KEY;
         private String requestHeader = null;
 
         @Bean
         public FilterRegistrationBean servletRegistrationBean() {
             final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-            final Slf4jMDCFilter log4jMDCFilterFilter = new Slf4jMDCFilter(responseHeader, mdcTokenKey, requestHeader);
+            final Slf4jMDCFilter log4jMDCFilterFilter = new Slf4jMDCFilter(responseHeader, requestHeader);
             registrationBean.setFilter(log4jMDCFilterFilter);
             registrationBean.setOrder(2);
             return registrationBean;
--- src/main/java/nl/appsource/latest/badge/filter/Slf4jMDCFilter.java
+++ src/main/java/nl/appsource/latest/badge/filter/Slf4jMDCFilter.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,19 +12,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class Slf4jMDCFilter extends OncePerRequestFilter {
 
+    private final static String MDCTOKENKEY = "Slf4jMDCFilter.UUID";
+
     private final String responseHeader;
-    private final String mdcTokenKey;
     private final String requestHeader;
 
+    public static Supplier<String> TOKEN = () -> MDC.get(MDCTOKENKEY);
+
     @Override
-    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
-            throws java.io.IOException, ServletException {
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws java.io.IOException, ServletException {
         try {
             final String token;
             if (!StringUtils.isEmpty(requestHeader) && !StringUtils.isEmpty(request.getHeader(requestHeader))) {
@@ -36,13 +38,13 @@ public class Slf4jMDCFilter extends OncePerRequestFilter {
 
             Thread.currentThread().setName(token);
 
-            MDC.put(mdcTokenKey, token);
+            MDC.put(MDCTOKENKEY, token);
             if (!StringUtils.isEmpty(responseHeader)) {
                 response.addHeader(responseHeader, token);
             }
             chain.doFilter(request, response);
         } finally {
-            MDC.remove(mdcTokenKey);
+            MDC.remove(MDCTOKENKEY);
         }
     }
 
