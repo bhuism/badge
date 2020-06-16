064e64e6910a3b341c6b5739f6284667d45179bc
Bas Huisman <bhuism@odee.net>
2020-06-16 10:50:03 +0200

    remove webflux

--- src/main/java/nl/appsource/latest/badge/BadgeApplication.java
+++ src/main/java/nl/appsource/latest/badge/BadgeApplication.java
@@ -2,11 +2,13 @@ package nl.appsource.latest.badge;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.boot.SpringApplication.run;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -14,6 +16,11 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @Slf4j
 @SpringBootApplication(proxyBeanMethods = false)
 public class BadgeApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Configuration(proxyBeanMethods = false)
     @EnableWebSecurity
