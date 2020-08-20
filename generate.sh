eb71064e48057a23b4e9dda3d84ad9adb0f3b258
Bas Huisman <bhuism@odee.net>
2020-08-20 14:21:26 +0200

    bump graal

--- generate.sh
+++ generate.sh
@@ -9,5 +9,5 @@ docker run -it --rm \
     --workdir /build \
     --volume "$HOME"/.m2:/root/.m2 \
     -p 8080:8080 \
-    oracle/graalvm-ce:20.1.0-java11 \
+    oracle/graalvm-ce:20.2.0-java11 \
     bash -c './build.sh ; $JAVA_HOME/bin/java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -Dspring.profiles.active=production -jar target/badge.jar'
